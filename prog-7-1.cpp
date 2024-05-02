a9f750a8a7dcff7938ab16b8885d2cc52fd8275d
mr-lobstr <y-po4tamishi@yandex.ru>
2024-05-03 00:37:56 +0700

    Лабораторная №8

---
+++ prog-7-1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int digit_add(long long num, int dig)
+{
+	while (num >= dig)
+	{
+		dig *= 10;
+	}
+
+	return dig + num;
+}
+
+bool func (long long N, long long sum, long long head, long long tail)
+{
+	if (head == 0)
+	{
+		if (sum + tail == N)
+			cout << tail;
+
+		return sum + tail == N;
+	}
+
+	if (func (N, sum + tail, head / 10, head % 10))
+	{
+		cout << "+" << tail;
+	}
+	else if (func (N, sum - tail, head / 10, head % 10))
+	{
+		cout << "-" << tail;
+	}
+	else if (func (N, sum, head / 10, digit_add(tail, head % 10)))
+	{}
+	else
+	{
+		return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	func (0, 0, 12345678, 9);
+}
---
+++ prog-7-2.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+size_t end_expr (const string& str, size_t ind)
+{
+	int brackets = 0;
+
+	for (; ind < str.size(); ++ind)
+	{
+		if (brackets == 0)
+		{
+			if (str[ind] == ',' or str[ind] == ')')
+				return ind;
+		}
+
+		if (str[ind] == ')')
+			--brackets;
+
+		if (str[ind] == '(')
+			++brackets;	
+	}
+
+	return str.size();
+}
+
+bool logic_func (const string& str, size_t ind)
+{
+	if (str[ind] == 't' or str[ind] == 'f')
+		return str[ind] == 't';
+
+	if (str[ind] == '!')
+		return not logic_func(str, ind + 2);
+
+	bool is_AND = str[ind] == '&';
+	ind += 2;
+
+	while (str[ind] != ')')
+	{
+		bool res = logic_func(str, ind + 1);
+
+		if (is_AND and not res)
+		{
+			return false;
+		}
+
+		if (not is_AND and res)
+		{
+			return true;
+		}
+
+		ind = end_expr(str, ind + 1);
+	}
+
+	return not is_AND;
+}
+
+int main()
+{
+	string st = "((((,))),)";
+	cout << logic_func("|(t,f)"s, 0) << endl;
+}
